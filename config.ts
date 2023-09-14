261dbcc6e4a5eef03e1259509d340f815bae3263
Seth Derrick <seth@sethderrick.tech>
2023-09-14 13:36:08 -0500

    Refactored app configuration file from main app.ts

---
+++ config.ts
@@ -0,0 +1,8 @@
+// Configuration for the application
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const BASE_URL = "https://api.krakenflex.systems/interview-tests-mock-api/v1/";
+export const API_KEY = process.env.API_KEY;
