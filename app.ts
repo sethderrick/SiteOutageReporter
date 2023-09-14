e083f43b17665cbbc3de5a3077dfa14329d6c62b
Seth Derrick <seth@sethderrick.tech>
2023-09-14 15:21:16 -0500

    Hoisted nested functions that were in main()

--- app.ts
+++ app.ts
@@ -9,13 +9,32 @@ interface SiteInfo {
     devices: Device[];  // List of associated devices
 }
 
+// Wrap each request with its own error handling function
+async function fetchOutages(): Promise<Outage[]> {
+    try {
+        return await tryRequest(() => get("outages"));
+    } catch (error) {
+        console.error("Error fetching outages:", error);
+        throw error;
+    }
+}
+
+async function fetchSiteInfo(): Promise<SiteInfo> {
+    try {
+        return await tryRequest(() => get("site-info/norwich-pear-tree"));
+    } catch (error) {
+        console.error("Error fetching site info:", error);
+        throw error;
+    }
+}
+
 export async function main() {
     try {
-        const outages: Outage[] = await get("outages");
-        const siteInfo: SiteInfo = await get(`site-info/norwich-pear-tree`);
+        // Start both get requests simultaneously with individual error handling
+        const [outages, siteInfo] = await Promise.all([fetchOutages(), fetchSiteInfo()]);
 
         const filteredOutages = outages
-            .filter((o: any) => new Date(o.begin) >= new Date("2022-01-01T00:00:00.000Z"));
+            .filter(o => new Date(o.begin) >= new Date("2022-01-01T00:00:00.000Z"));
 
         const outagesToReport = filteredOutages.filter(outage =>
             siteInfo.devices.some(device => device.id === outage.id)
@@ -23,9 +42,10 @@ export async function main() {
             const matchingDevice = siteInfo.devices.find(device => device.id === outage.id);
             return { ...outage, name: matchingDevice?.name };
         });
+
         const postResult = await post("site-outages/norwich-pear-tree", outagesToReport);
     } catch (error) {
-        console.error(error);
+        console.error("An error occurred in main function:", error);
     }
 }
 
